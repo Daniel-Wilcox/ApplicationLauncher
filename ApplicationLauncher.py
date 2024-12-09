07c2685676449d8951808ddc6d5fc5af5bf4788f
Dan <d.dt.wilcox@gmail.com>
2024-12-09 13:23:53 +0200

    Added window to applucation Launcher

--- ApplicationLauncher.py
+++ ApplicationLauncher.py
@@ -1,5 +1,20 @@
+import tkinter as tk
+
+
+class Application(tk.Tk):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Add window properties
+        self.title("Application Launcher")
+        self.geometry("600x200")
+        self.eval("tk::PlaceWindow . center")
+        self.focus_force()
+
+
 def application_launcher():
-    pass
+    app = Application()
+    app.mainloop()
 
 
 if __name__ == "__main__":
