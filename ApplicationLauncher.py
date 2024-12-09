6f7e16648f53ac8875fa614551d76471e0407838
Dan <d.dt.wilcox@gmail.com>
2024-12-09 13:40:43 +0200

    added first view and changed application class to controller class

--- ApplicationLauncher.py
+++ ApplicationLauncher.py
@@ -1,12 +1,57 @@
 import tkinter as tk
 
+LARGE_FONT = ("Verdana", 12)
 
-class Application(tk.Tk):
+
+class StartView(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        # Define locals
+        self.controller = controller
+
+        # Initialise Widgets
+        self.load_widgets()
+
+    def load_widgets(self):
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(
+            self,
+            text="Visit Page 1",
+            command=lambda: print("Changing to 'PageOneView'."),
+        )
+        button1.pack()
+
+        button2 = tk.Button(
+            self,
+            text="Visit Page 2",
+            command=lambda: print("Changing to 'PageTwoView'."),
+        )
+        button2.pack()
+
+    def reset_defaults(self):
+        pass
+
+
+class ApplicationController(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         # Add window properties
         self.set_window_properties()
+
+        # Set 'main frame' to be parent of all application views
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(side="top", fill="both", expand=True)
+        self.main_frame.grid_rowconfigure(0, weight=1)
+        self.main_frame.grid_columnconfigure(0, weight=1)
+
+        # Add start view to application's main frame
+        first_page = StartView(parent=self.main_frame, controller=self)
+        first_page.grid(row=0, column=0, sticky="nsew")
+        first_page.tkraise()
 
     def set_window_properties(self):
         self.title("Application Launcher")
@@ -16,7 +61,9 @@ class Application(tk.Tk):
 
 
 def application_launcher():
-    app = Application()
+
+    # Todo: Add in configuration capabilities for
+    app = ApplicationController()
     app.mainloop()
 
 
